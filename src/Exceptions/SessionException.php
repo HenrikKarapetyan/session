a6f543b306a08b0194c8b441de8b9a39ad18784c
Henrik Karapetyan <g.karapetyan@tool-kit.tech>
2024-06-09 21:15:29 +0400

    refactored code

--- src/Exceptions/SessionException.php
+++ src/Exceptions/SessionException.php
@@ -5,8 +5,15 @@ declare(strict_types=1);
 namespace Henrik\Session\Exceptions;
 
 use Exception;
+use Throwable;
 
 /**
  * Class SessionException.
  */
-class SessionException extends Exception {}
+class SessionException extends Exception
+{
+    public function __construct(string $message = '', int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
